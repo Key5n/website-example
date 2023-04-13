72d6cbb66642d1073ee09bee58693a14765fd1df
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-04-14 06:44:56 +0900

    add font and animation

--- js/main.js
+++ js/main.js
@@ -7,4 +7,23 @@ navLinks?.addEventListener("click", () => {
   navLinks?.classList.toggle("expanded");
 });
 
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("emerging");
+    }
+    console.log(entry);
+  });
+};
+
+const options = {
+  threshold: 1,
+};
+
+const targetList = [".gallery", ".about", ".twitter"];
+
+const observer = new IntersectionObserver(callback, options);
+targetList.forEach((value) => {
+  observer.observe(document.querySelector(value));
+});
 import "https://platform.twitter.com/widgets.js";
